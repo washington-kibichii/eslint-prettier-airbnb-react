9d6918d51b04b0f7d41febcb954925c23df5b26f
Washington Kibichii <38534263+washington-kibichii@users.noreply.github.com>
2021-01-10 13:08:01 +0300

    Update eslint-prettier-config.sh

--- eslint-prettier-config.sh
+++ eslint-prettier-config.sh
@@ -53,7 +53,7 @@ finished=false
 
 # Max Line Length Prompt
 while ! $finished; do
-  read -p "What max line length do you want to set for ESLint and Prettier? (Recommendation: 100)"
+  read -p "What max line length do you want to set for ESLint and Prettier? (Recommendation: 80)"
   if [[ $REPLY =~ ^[0-9]{2,3}$ ]]; then
     max_len_val=$REPLY
     finished=true
@@ -68,12 +68,27 @@ echo "What style of trailing commas do you want to enforce with Prettier?"
 echo -e "${YELLOW}>>>>> See https://prettier.io/docs/en/options.html#trailing-commas for more details.${NC}"
 select trailing_comma_pref in "none" "es5" "all"; do
   case $trailing_comma_pref in
-    "none" ) break;;
-    "es5" ) break;;
-    "all" ) break;;
+    none ) break;;
+    es5 ) break;;
+    all ) break;;
   esac
 done
 echo
+
+# Checks for existing prettierrc files
+if [ -f ".prettierrc.js" -o -f "prettier.config.js" -o -f ".prettierrc.yaml" -o -f ".prettierrc.yml" -o -f ".prettierrc.json" -o -f ".prettierrc.toml" -o -f ".prettierrc" ]; then
+  echo -e "${RED}Existing Prettier config file(s) found${NC}"
+  ls -a | grep "prettier*" | xargs -n 1 basename
+  echo
+  echo -e "${RED}CAUTION:${NC} The configuration file will be resolved starting from the location of the file being formatted, and searching up the file tree until a config file is (or isn't) found. https://prettier.io/docs/en/configuration.html"
+  echo
+  read -p  "Write .prettierrc${config_extension} (Y/n)? "
+  if [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo -e "${YELLOW}>>>>> Skipping Prettier config${NC}"
+    skip_prettier_setup="true"
+  fi
+  echo
+fi
 
 # ----------------------
 # Perform Configuration
@@ -82,17 +97,17 @@ echo
 echo -e "${GREEN}Configuring your development environment... ${NC}"
 
 echo
-echo -e "1/4 ${LCYAN}ESLint & Prettier Installation... ${NC}"
+echo -e "1/5 ${LCYAN}ESLint & Prettier Installation... ${NC}"
 echo
 $pkg_cmd -D eslint prettier eslint-plugin-react-hooks
 
 echo
-echo -e "2/4 ${YELLOW}Conforming to Airbnb's JavaScript Style Guide... ${NC}"
+echo -e "2/5 ${YELLOW}Conforming to Airbnb's JavaScript Style Guide... ${NC}"
 echo
 $pkg_cmd -D eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react babel-eslint
 
 echo
-echo -e "3/4 ${LCYAN}Making ESlint and Prettier play nice with each other... ${NC}"
+echo -e "3/5 ${LCYAN}Making ESlint and Prettier play nice with each other... ${NC}"
 echo "See https://github.com/prettier/eslint-config-prettier for more details."
 echo
 $pkg_cmd -D eslint-config-prettier eslint-plugin-prettier
@@ -102,7 +117,7 @@ if [ "$skip_eslint_setup" == "true" ]; then
   break
 else
   echo
-  echo -e "4/4 ${YELLOW}Building your .eslintrc${config_extension} file...${NC}"
+  echo -e "4/5 ${YELLOW}Building your .eslintrc${config_extension} file...${NC}"
   > ".eslintrc${config_extension}" # truncates existing file (or creates empty)
 
   echo ${config_opening}'
@@ -128,10 +143,17 @@ else
     "jest": true
   },
   "rules": {
+    "react/prop-types": 0,
     "react-hooks/rules-of-hooks": "error",
     "no-debugger": 0,
     "no-alert": 0,
-    "no-unused-vars": 1,
+    "no-unused-vars": [
+      1,
+      {
+        "ignoreSiblings": true,
+        "argsIgnorePattern": "res|next|^err"
+      }
+    ],
     "prefer-const": [
       "error",
       {
@@ -155,11 +177,12 @@ else
       }
     ],
     "no-console": 0,
-    "import/prefer-default-export": 1,
+    "import/prefer-default-export": 0,
     "import": 0,
     "func-names": 0,
     "space-before-function-paren": 0,
     "comma-dangle": 0,
+    "class-methods-use-this": 0,
     "max-len": 0,
     "import/extensions": 0,
     "no-underscore-dangle": 0,
@@ -183,7 +206,20 @@ else
       }
     ],
     "radix": 0,
-    "no-shadow": "off",
+    "no-shadow": [
+      2,
+      {
+        "hoist": "all",
+        "allow": [
+          "resolve",
+          "reject",
+          "done",
+          "next",
+          "err",
+          "error"
+        ]
+      }
+    ],
     "quotes": [
       2,
       "single",
@@ -195,9 +231,9 @@ else
     "prettier/prettier": [
       "error",
       {
-        "trailingComma": "'${trailing_comma_pref}'",
+        "trailingComma": "es5",
         "singleQuote": true,
-        "printWidth": '${max_len_val}'
+        "printWidth": 80
       }
     ],
     "jsx-a11y/href-no-hash": "off",
@@ -213,11 +249,25 @@ else
   "plugins": [
     "prettier",
     "react",
-    "react-hooks"
+    "react-hooks",
   ]
 }' >> .eslintrc${config_extension}
 fi
 
+
+if [ "$skip_prettier_setup" == "true" ]; then
+  break
+else
+  echo -e "5/5 ${YELLOW}Building your .prettierrc${config_extension} file... ${NC}"
+  > .prettierrc${config_extension} # truncates existing file (or creates empty)
+
+  echo ${config_opening}'
+  "printWidth": '${max_len_val}',
+  "singleQuote": true,
+  "trailingComma": "'${trailing_comma_pref}'"
+}' >> .prettierrc${config_extension}
+fi
+
 echo
 echo -e "${GREEN}Finished setting up!${NC}"
-echo
+ech0
